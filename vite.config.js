c9482e9042c61fb60b99ec5007031177817f6b31
ryanharrs <ryanharrs@gmail.com>
2025-08-16 23:04:41 -0400

    get FE up

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    host: true
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true
   }
 })
